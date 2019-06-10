7832e00d2dcaa3389fd07961fb2d1d5794254f29
Larissa Hoshiko Suganuma <lala94032@gmail.com>
2019-06-10 16:16:45 -0700

    commit changes

---
+++ src/app/app-grocery/app-grocery.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-grocery',
+  templateUrl: './app-grocery.component.html',
+  styleUrls: ['../../assets/bootstrap-4.3.1-dist 2/css/bootstrap.min.css']
+})
+
+export class AppGroceryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import{FormsModule} from '@angular/forms';
+import { HttpModule} from '@angular/http';
+import { AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppGroceryComponent } from './app-grocery/app-grocery.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppGroceryComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppGroceryComponent]
 })
 export class AppModule { }
